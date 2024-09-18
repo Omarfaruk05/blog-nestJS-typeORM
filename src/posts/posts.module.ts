16fa78c33a15264174fd4055086cd20d2520baed
Md. Omar Faruk <mdomarfaruk149518@gmail.com>
2024-09-18 16:35:41 +0600

    [Done]get metaOptions

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,10 +3,12 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { UsersService } from 'src/users/users.service';
+import { MetaOption } from 'src/meta-options/entities/metaOption.entity';
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService],
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post, MetaOption])],
 })
 export class PostsModule {}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,15 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { UsersService } from 'src/users/users.service';
+import { Repository } from 'typeorm';
+import { Post } from './entities/post.entity';
+import { MetaOption } from 'src/meta-options/entities/metaOption.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    /**
+     * Injecting postRepository
+     */
+    @InjectRepository(Post)
+    private readonly postsRepository: Repository<Post>,
+  ) {}
+
+  public async create(createPostDto: CreatePostDto) {
+    // Create post
+    let post = this.postsRepository.create(createPostDto);
+
+    // return the post
+    return await this.postsRepository.save(post);
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  public async findAll() {
+    const posts = await this.postsRepository.find({
+      relations: {
+        metaOptions: true,
+      },
+    });
+    return posts;
   }
 
   findOne(id: number) {
